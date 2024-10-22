34c331fce65074a9fb442a4f249bcced0a7a84da
unknown <mrcinpuy@gmail.com>
2024-10-22 22:19:25 +0700

    rename database

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
-<?php 
-$koneksi=mysqli_connect("localhost","root","");
+<?php
+$koneksi = mysqli_connect("localhost", "root", "");
 //$koneksi=mysqli_connect("localhost:3306","root","root");
-$db=mysqli_select_db($koneksi,"db_kasirapp");
+$db = mysqli_select_db($koneksi, "db-kasir");
 date_default_timezone_set('Asia/Jakarta');
-?>
